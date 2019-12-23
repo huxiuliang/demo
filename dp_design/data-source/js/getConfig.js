14187ea1e989a5639262479db82a1b0204be14f0
huxiuliang <86138151@qq.com>
2019-12-23 23:21:49 +0800

    init

--- dp_design/data-source/js/getConfig.js
+++ dp_design/data-source/js/getConfig.js
@@ -27,24 +27,7 @@ function reqServerController(path, dataparm, callback) {
     });
 }
 
-function reqServerControllerParms(path, dataparm, callback) {
-    var rdpserver = getConfigProp("rdpserver");
-    $.ajax({
-        url: rdpserver + path,
-        type: 'post',
-        data: dataparm,
-        success: function (data) {
-            if (callback && typeof (callback) == "function") {
-                callback.call(this, data);
-            }
-        },
-        error: function (e) {
-            if (callback && typeof (callback) == "function") {
-                callback.call(this, e);
-            }
-        }
-    });
-}
+
 
 function tableValsToNodeData(tableData) {
     var zNodes = [];
---
+++ dp_design/web/requestTools.js
@@ -0,0 +1,40 @@
+function reqServerController(path, dataparm, callback) {
+    var rdpserver = "";//getConfigProp("rdpserver");
+    $.ajax({
+        url: rdpserver + path,
+        type: 'post',
+        data: JSON.stringify(dataparm),
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        success: function (data) {
+            if (callback && typeof (callback) == "function") {
+                callback.call(this, data);
+            }
+        },
+        error: function (e) {
+            if (callback && typeof (callback) == "function") {
+                callback.call(this, e);
+            }
+        }
+    });
+}
+
+function reqServerControllerParms(path, dataparm, callback) {
+    var rdpserver = getConfigProp("rdpserver");
+    $.ajax({
+        url: rdpserver + path,
+        type: 'post',
+        data: dataparm,
+        success: function (data) {
+            if (callback && typeof (callback) == "function") {
+                callback.call(this, data);
+            }
+        },
+        error: function (e) {
+            if (callback && typeof (callback) == "function") {
+                callback.call(this, e);
+            }
+        }
+    });
+}
